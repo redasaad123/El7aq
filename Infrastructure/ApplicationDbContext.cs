154dcb69c64dac21e2a9a86de7251ee6e0f7df65
A-Qassem <aqassemm2004@gmail.com>
2025-09-03 14:55:14 +0300

    0

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,32 +1,27 @@
-﻿
-using Core.Entities;
+﻿using Core.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Reflection.Emit;
-using System.Reflection.Metadata;
 
-namespace Infrastructure
+namespace Infrastructure;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
-    public class ApplicationDbContext : IdentityDbContext<AppUsers>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        {
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
 
-            builder.Entity<AppUsers>().ToTable("users", "security");
-            builder.Entity<IdentityRole>().ToTable("Roles", "security");
-            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole", "security");
-            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim", "security");
-            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin", "security");
-            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim", "security");
-            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken", "security");
-            
-        }
+        builder.Entity<AppUsers>().ToTable("users", "security");
+        builder.Entity<IdentityRole>().ToTable("Roles", "security");
+        builder.Entity<IdentityUserRole<string>>().ToTable("UserRole", "security");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim", "security");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin", "security");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim", "security");
+        builder.Entity<IdentityUserToken<string>>().ToTable("UserToken", "security");
+
     }
 }
